8b2e991c66adb915540bb49ef37f1a4dccdab165
vinaypattanshetti94 <126855488+vinaypattanshetti94@users.noreply.github.com>
2023-05-31 19:32:58 +0530

    Add files via upload

---
+++ DemoQA.cs
@@ -0,0 +1,117 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V111.Input;
+using OpenQA.Selenium.Interactions;
+using System.Xml.Linq;
+
+namespace CaseStudy
+{
+    [TestFixture]
+    public class DemoQA
+    {
+        private IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://demoqa.com/");
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void Elements()
+        {
+            WebElements webElements = new WebElements(driver);
+            webElements.NavigateToCard("elements");
+            webElements._textBoxButton.Click();
+            webElements._userNameTextBox.SendKeys("Vinay");
+            webElements._emailTextBox.SendKeys("name@gmail.com");
+            webElements._currentAddressTextBox.SendKeys("#1307,KHB Platnium");
+            webElements._permanentAddressTextBox.SendKeys("#1307,KHB Platnium");
+            webElements._submitButton.SendKeys(Keys.Enter);
+            Assert.True(webElements._nameLabel.Text.Contains("Vinay"));
+            Assert.True(webElements._emailLabel.Text.Contains("name@gmail.com"));
+            Assert.True(webElements._currentAddressLabel.Text.Contains("KHBPlatnium"));
+            Assert.True(webElements._permanentAddressLabel.Text.Contains("KHBPlatnium"));
+
+            webElements.NavigateToCard("checkbox");
+            webElements._homeCheckBox.SendKeys(Keys.Enter);
+            Assert.True(webElements._homeCheckBox.Selected);
+
+            webElements.NavigateToCard("webtables");
+            webElements._addNewRecordButton.Click();
+            webElements.RegistrationForm("Vinay","P","v@gmail.com","23","1000","MCA");
+
+            webElements.NavigateToCard("buttons");
+            Actions actions = new Actions(driver);
+            actions.ContextClick(webElements._rightClickButton).Perform();
+            Assert.True(webElements._rightClickLabel.Text.Contains("You have done a right click"));
+
+            webElements.NavigateToCard("upload-download");
+            webElements._chooseFileButton.SendKeys("C:\\Users\\pattansh-1\\Downloads\\Vinay_Travel\\BusTicket_Return.pdf");
+            webElements._downloadButton.Click();
+
+            webElements.NavigateToCard("alerts");
+            webElements._confirmButtonButton.Click();
+            IAlert simpleAlert = driver.SwitchTo().Alert();
+            simpleAlert.Accept();
+            driver.SwitchTo().DefaultContent();
+
+            webElements.NavigateToCard("auto-complete");
+            webElements._autoCompleteSingleContainer.SendKeys("Red");
+            webElements._autoCompleteSingleContainer.SendKeys(Keys.ArrowDown);
+
+            webElements.NavigateToCard("date-picker");
+            webElements._datePickerMonthYearInput.SendKeys("05312023");
+
+            webElements.NavigateToCard("tool-tips");
+            Actions builder = new Actions(driver);
+            builder.MoveToElement(webElements._toolTipButton)
+            .Click().Build().Perform();
+
+            webElements.NavigateToCard("droppables");
+            builder = new Actions(driver);
+            var dragAndDrop = builder.ClickAndHold(webElements._draggable).MoveToElement(webElements._droppable).Release(webElements._draggable).Build();
+            dragAndDrop.Perform();
+
+            webElements.NavigateToCard("browser-windows");
+            webElements._newTabButton.Click();
+            driver.SwitchTo().Window(driver.WindowHandles[1]);
+            Console.WriteLine("Current window id: " + driver.CurrentWindowHandle);
+            Console.WriteLine("Page title in second tab is: " + driver.Title);
+            driver.Close();
+            driver.SwitchTo().Window(driver.WindowHandles[0]);
+
+            webElements = new WebElements(driver);
+            webElements._newWindowButton.Click();
+            string newWindowHandle = driver.WindowHandles.Last();
+            var newWindow = driver.SwitchTo().Window(newWindowHandle);
+
+            string expectedNewWindowTitle = "https://demoqa.com/sample";
+            Assert.AreEqual(expectedNewWindowTitle, newWindow.Title);
+            driver.SwitchTo().DefaultContent();
+
+        }
+
+        [Test]
+        public void BookStore()
+        {
+            WebElements webElements = new WebElements(driver);
+            webElements.NavigateToCard("books");
+            webElements._loginButton.Click();
+            webElements._BookUserNameTextBox.SendKeys("VinayP");
+            webElements._BookPasswordTextBox.SendKeys("Vinay@2023");
+            webElements._loginButton.Click();
+            Assert.True(webElements._userNameValue.Text.Contains("VinayP"), "User is not logged in");
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
---
+++ WebElements.cs
@@ -0,0 +1,152 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseStudy
+{
+    public class WebElements
+    {
+        private IWebDriver driver;
+
+        [FindsBy(How = How.Id, Using = "userName")]
+        public IWebElement _userNameTextBox;
+
+        [FindsBy(How = How.Id, Using = "userEmail")]
+        public IWebElement _emailTextBox;
+
+        [FindsBy(How = How.Id, Using = "currentAddress")]
+        public IWebElement _currentAddressTextBox;
+
+        [FindsBy(How = How.Id, Using = "permanentAddress")]
+        public IWebElement _permanentAddressTextBox;
+
+        [FindsBy(How = How.Id, Using = "submit")]
+        public IWebElement _submitButton;
+
+        [FindsBy(How = How.XPath, Using = "//ul[@class='menu-list']//li[@id='item-0']")]
+        public IWebElement _textBoxButton;
+
+        [FindsBy(How = How.Id, Using = "permanentAddress")]
+        public IWebElement _permanentAddressLabel;
+
+        [FindsBy(How = How.Id, Using = "name")]
+        public IWebElement _nameLabel;
+
+        [FindsBy(How = How.Id, Using = "email")]
+        public IWebElement _emailLabel;
+
+        [FindsBy(How = How.Id, Using = "currentAddress")]
+        public IWebElement _currentAddressLabel;
+
+        [FindsBy(How = How.Id, Using = "tree-node-home")]
+        public IWebElement _homeCheckBox;
+
+        [FindsBy(How = How.Id, Using = "addNewRecordButton")]
+        public IWebElement _addNewRecordButton;
+
+        [FindsBy(How = How.Id, Using = "rightClickBtn")]
+        public IWebElement _rightClickButton;
+
+        [FindsBy(How = How.Id, Using = "rightClickMessage")]
+        public IWebElement _rightClickLabel;
+
+        [FindsBy(How = How.Id, Using = "uploadFile")]
+        public IWebElement _chooseFileButton;
+
+        [FindsBy(How = How.Id, Using = "downloadButton")]
+        public IWebElement _downloadButton;
+
+        [FindsBy(How = How.Id, Using = "tabButton")]
+        public IWebElement _newTabButton;
+
+        [FindsBy(How = How.Id, Using = "windowButton")]
+        public IWebElement _newWindowButton;
+
+        [FindsBy(How = How.Id, Using = "confirmButton")]
+        public IWebElement _confirmButtonButton;
+
+        [FindsBy(How = How.Id, Using = "autoCompleteSingleContainer")]
+        public IWebElement _autoCompleteSingleContainer;
+
+        [FindsBy(How = How.Id, Using = "datePickerMonthYearInput")]
+        public IWebElement _datePickerMonthYearInput;
+
+        [FindsBy(How = How.Id, Using = "toolTipButton")]
+        public IWebElement _toolTipButton;
+
+        [FindsBy(How = How.Id, Using = "draggable")]
+        public IWebElement _draggable;
+
+        [FindsBy(How = How.Id, Using = "droppable")]
+        public IWebElement _droppable;
+
+        [FindsBy(How = How.Id, Using = "login")]
+        public IWebElement _loginButton;
+
+        [FindsBy(How = How.Id, Using = "userName")]
+        public IWebElement _BookUserNameTextBox;
+
+        [FindsBy(How = How.Id, Using = "password")]
+        public IWebElement _BookPasswordTextBox;
+
+        [FindsBy(How = How.Id, Using = "userName-value")]
+        public IWebElement _userNameValue;
+
+        public WebElements(IWebDriver webDriver) 
+        { 
+            this.driver = webDriver;
+            PageFactory.InitElements(webDriver, this);
+        }
+
+        public void MenuClick(int index)
+        {
+            driver.FindElement(By.XPath(String.Format("//ul[@class='menu-list']//li[@id='item-{0}']),index"))).Click();
+            
+        }
+
+        public void NavigateToCard(string card)
+        {
+            driver.Navigate().GoToUrl(string.Format("https://demoqa.com/{0}", card));
+        }
+
+        public void RegistrationForm(string firstName, string lastName, string email, string age, string salary, string dept)
+        {
+            driver.FindElement(By.Id("firstName")).Clear();
+
+            driver.FindElement(By.Id("firstName")).SendKeys(firstName);
+
+            driver.FindElement(By.Id("lastName")).Clear();
+
+            driver.FindElement(By.Id("lastName")).SendKeys(lastName);
+
+
+            driver.FindElement(By.Id("userEmail")).Clear();
+
+            driver.FindElement(By.Id("userEmail")).SendKeys(email);
+
+
+            driver.FindElement(By.Id("age")).Clear();
+
+            driver.FindElement(By.Id("age")).SendKeys(age);
+
+
+            driver.FindElement(By.Id("salary")).Clear();
+
+            driver.FindElement(By.Id("salary")).SendKeys(salary);
+
+            driver.FindElement(By.Id("department")).Clear();
+
+            driver.FindElement(By.Id("department")).SendKeys(dept);
+
+            driver.FindElement(By.Id("submit")).Click();
+
+        }
+
+
+    }
+}
